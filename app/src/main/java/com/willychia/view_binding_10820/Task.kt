e4b85158782f8196e024a3ccd4cc66b3a9a392e8
CroZie19 <willychristianchia12@gmail.com>
2022-09-15 08:30:27 +0700

    Initial commit

---
+++ app/src/main/java/com/willychia/view_binding_10820/Task.kt
@@ -0,0 +1,3 @@
+package com.willychia.view_binding_10820
+
+data class Task(val chapter:String, val title:String, val details:String)
---
+++ app/src/main/java/com/willychia/view_binding_10820/TaskList.kt
@@ -0,0 +1,11 @@
+package com.willychia.view_binding_10820
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("Chapter 1", "Bangun Tidur", "Tidur lagi , bangun makan mandi dan tidur lagi. hanya bisa terjadi ketika hari libur"),
+        Task("Chapter 2", "Main Basket", "Main Basket tiap hari jumat FTI di kampus 1 UAJY"),
+        Task("Chapter 3", "Ngoding", "Tiap minggu harus ngulik kodingan PBP yang bikin pusing tiap minggu:)"),
+        Task("Chapter 4", "Kuliah", "Kuliah tiap hari dari selasa sampai jumat, jangan lupa lewat FBE(U KNOW LAH, CUCI MATA DULU YA GES YA:v)"),
+        Task("Chapter 5", "Galau", "Galau kena tugas yang banyak, sama galau juga LDRan:)")
+    )
+}
